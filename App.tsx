e934a142a9e65b6d31add1e0906e8c3e5cab2d9a
user07-ioemlak <user07-ioemlak@users.noreply.github.com>
2026-08-11 11:21:05 +0300

    Derleme_Guvenlik_Tasarim

--- App.tsx
+++ App.tsx
@@ -253,7 +253,7 @@ const styles = StyleSheet.create({
     left: 0,
     right: 0,
     bottom: 0,
-    backgroundColor: 'rgba(255, 255, 255, 0.9)',
+    backgroundColor: '#FFFFFF',
     justifyContent: 'center',
     alignItems: 'center',
     zIndex: 10,
